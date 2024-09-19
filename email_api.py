edb89d5ecc03ebe948e319b8e24676b496585810
arvin <arvinsaguisa@gmail.com>
2024-09-20 01:07:30 +0800

    this is a test

---
+++ email_api.py
@@ -0,0 +1,38 @@
+# import sa email api
+from flask_mail import Mail, Message
+from flask import Flask
+
+#get the variables from .env file
+from dotenv import load_dotenv
+import os
+
+#load them in this file
+load_dotenv()
+
+# declare og variables para sa email api (values from env)
+EMAIL_SERVER_NI_SHA = os.getenv("MAIL_SERVER")
+EMAIL_PORT_NI_SHA = os.getenv("MAIL_PORT")
+EMAIL_USE_TLS_NI = os.getenv("MAIL_USE_TLS")
+EMAIL_USERNAME = os.getenv("MAIL_USERNAME")
+EMAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+EMAIL_SENDER_CREDISYNC = os.getenv("MAIL_DEFAULT_SENDER")
+
+app = Flask(__name__) 
+
+# Mail configuration
+app.config['MAIL_SERVER'] = EMAIL_SERVER_NI_SHA
+app.config['MAIL_PORT'] = EMAIL_PORT_NI_SHA
+app.config['MAIL_USE_TLS'] = EMAIL_USE_TLS_NI
+app.config['MAIL_USERNAME'] = EMAIL_USERNAME
+app.config['MAIL_PASSWORD'] = EMAIL_PASSWORD
+app.config['MAIL_DEFAULT_SENDER'] = EMAIL_SENDER_CREDISYNC
+
+mail = Mail(app)
+
+def send_approval_email(recipient):
+    subject = "Approval Notification"
+    body = "Your request has been approved!"
+    
+    msg = Message(subject, recipients=[recipient])
+    msg.body = body
+    mail.send(msg)
